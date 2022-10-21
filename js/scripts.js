0debfe138da982088bef3dd78050b46c4e98017b
Richelle Thorpe <richelle.ak@gmail.com>
2022-10-21 09:40:27 -0700

    add label and imput to form elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+function hideResults(){
 
 
+}
+
+
+
+window.addEventListener("load", function () {
+
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    hideResults();
+    const bDay = document.querySelector("input#born").value;
+
+
+  })
+}
+
+)
 
