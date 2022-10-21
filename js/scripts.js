dfbc0706544c1d9b6d70ed34143bbc3e2747d433
Richelle Thorpe <richelle.ak@gmail.com>
2022-10-21 10:21:56 -0700

    BUGFIX: submit event listener

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,11 @@ window.addEventListener("load", function () {
     event.preventDefault();
     hideResults();
     const bDay = document.querySelector("input#born").value;
+    const radioButton = document.querySelector("input[name='lifeStyleType']:checked").value;
+    const income =document.getElementById("money").value;
+    const 
+
+
 
 
   })
